13e50261c81c0a33e6340e8fe8ac3e5c28aaaeea
Cho Lin Tsai <max19851113@gmail.com>
2017-12-22 16:50:34 +0800

    header section finishied

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,22 @@
 const css = require('../css/app.scss');
 
-const viewShoe = document.getElementById('btn-view');
-
+const btnView = document.getElementById('btn-view');
+const btnViewClose = document.getElementsByClassName('closebtn')[0]
 
 
 function showShoe() {
+	let hidenShoe = document.getElementsByClassName('slide-in')[0];
+	hidenShoe.classList.add('active');
+}
 
+function hideShoe() {
+	let shownShoe = document.getElementsByClassName('active')[0];
+	shownShoe.classList.remove('active');
 }
 
 
+// header view btn click function to show Shoe
+btnView.addEventListener('click', showShoe);
 
-viewShoe.addEventListener('click', showShoe);
+// header close btn click function to close Shoe
+btnViewClose.addEventListener('click', hideShoe);
