a045b176958e36c4d9b9914995c206bb70805def
Cho Lin Tsai <max19851113@gmail.com>
2018-01-10 22:02:12 +0800

    all sections are finished, started rwd

--- src/js/app.js
+++ src/js/app.js
@@ -30,6 +30,16 @@ function $select(element) {
 	return document.querySelector(element);
 }
 
+// console.log
+function $log(x){
+	return console.log(x);
+}
+
+// get element offsetTop
+function OffsetTop(x) {
+	return $log(x.offsetTop);
+}
+
 // function for section phase to reuse
 function sectionSwitch(selectEle, eleClass, indexNum) {
 	switch (indexNum) {
@@ -61,7 +71,7 @@ function checkScroll() {
 	*  Header section javascript
 	**/
 	// get window scrollY value
-	let wScrollY = window.top.scrollY;
+	let wScrollY = window.scrollY;
 	// set event trigger value
 	let triggerHeight = 200;
 	// get #header h1 and .header-h1-slide-right
@@ -75,40 +85,49 @@ function checkScroll() {
 	// get #header-arrow and arrow and arrow-animation
 	let headerArrow = $select('.header-arrow');
 	let arrowFadeOut = 'header-arrow-fade-out'
-	let arrowAnimation = 'arrow-animation'
+	let arrowAnimation = 'arrow-animation';
+
+	let headerEleTrigger = wScrollY > (headerArrow.offsetTop * 0.1);
 
 	// check if true to add|remove .h1SlideRight .pSlideRight
-	if( wScrollY >= triggerHeight ) {
-		$addClsls(headerH1, h1SlideRight)
-		$addClsls(headerP, pSlideRight)
+	if(headerEleTrigger) {
+		$addClsls(headerH1, h1SlideRight);
+		$addClsls(headerP, pSlideRight);
+		$addClsls(headerArrow, arrowFadeOut);
+		$rmClsls(headerArrow, arrowAnimation);
 	} else {
 		$rmClsls(headerH1, h1SlideRight);
 		$rmClsls(headerP, pSlideRight);
+		$rmClsls(headerArrow, arrowFadeOut);
+		$addClsls(headerArrow, arrowAnimation);
 	}
 
 	// check if true to fade out #header-arrow
-	if ( wScrollY >= triggerHeight / 2  ) {
-		$addClsls(headerArrow, arrowFadeOut);
-	} else {
-		$rmClsls(headerArrow, arrowFadeOut)
-	}
+	// if ( wScrollY >= triggerHeight / 2  ) {
+	// 	$addClsls(headerArrow, arrowFadeOut);
+	// } else {
+	// 	$rmClsls(headerArrow, arrowFadeOut)
+	// }
 
 	// check if true to stop arrow animation before arrow fadeout
-	if (wScrollY >= triggerHeight / 2.1 ) {
-		$rmClsls(headerArrow, arrowAnimation);
-	} else {
-		$addClsls(headerArrow, arrowAnimation);
-	}
+	// if (wScrollY >= triggerHeight / 2.1 ) {
+	// 	$rmClsls(headerArrow, arrowAnimation);
+	// } else {
+	// 	$addClsls(headerArrow, arrowAnimation);
+	// }
 
 	/**
 	* Aside section javascript
 	**/
+
+	let asideContainer = $select('#aside-container');
 	let asideSection2 = $select('#aside-section-2');
 	let asideSection3 = $select('#aside-section-3');
-	let asideTriggerPoint = 300;
+	let asideOffsetTop = asideContainer.offsetTop;
+	let asideTrigger = wScrollY > (asideOffsetTop * 0.1)
 	let asideDelay = 200;
 	// check if true to show aside content
-	if (wScrollY > asideTriggerPoint) {
+	if (asideTrigger) {
 		$rmClsls(asideSection2, 'set-to-left');
 		setTimeout( () => {
 			$rmClsls(asideSection3, 'set-to-right');
@@ -119,126 +138,177 @@ function checkScroll() {
 	*  main section javascript
 	**/
 	// set trigger point and get #main-p
-	let mainWrapper = $select('#main-wrapper');
-	let mainPtoShow = 900;
-	let mainPtoHide = 1650;
 	let mainP = $select('#main-p');
-	// check if ture to show #main-wrapper(fixed-grid)
-	if (wScrollY < 850) {
-		$addClsls(mainWrapper, 'hidden');
-	} else if ( wScrollY > 2000 ){
-		$addClsls(mainWrapper, 'hidden');
+
+	let mainContenShow = wScrollY > asideOffsetTop * 0.95;
+
+	if (mainContenShow) {
+		$rmClsls(mainP, 'main-p-offsetY', 'main-p-offsetY-up');
 	} else {
-		$rmClsls(mainWrapper, 'hidden');
+		$addClsls(mainP, 'main-p-offsetY', 'main-p-offsetY-up');
 	}
 
-	// check conditions to show/hide main-p
-	switch (true) {
-		// control #main-p show/hide
-		case wScrollY > mainPtoShow && wScrollY < mainPtoHide:
-			$rmClsls(mainP, 'main-p-offsetY', 'main-p-offsetY-up')
-			break;
-		case wScrollY < mainPtoShow:
-			$addClsls(mainP, 'main-p-offsetY');
-			break;
-		case wScrollY > mainPtoHide:
-			$addClsls(mainP, 'main-p-offsetY-up');
-			break;
-		default:
-	}
+	// check if ture to show #main-wrapper(fixed-grid)
+	// if (wScrollY < 850) {
+	// 	$addClsls(mainWrapper, 'hidden');
+	// } else if ( wScrollY > 2000 ){
+	// 	$addClsls(mainWrapper, 'hidden');
+	// } else {
+	// 	$rmClsls(mainWrapper, 'hidden');
+	// }
+
+	//check conditions to show/hide main-p
+	// switch (true) {
+	// 	// control #main-p show/hide
+	// 	case wScrollY > mainPtoShow && wScrollY < mainPtoHide:
+	// 		$rmClsls(mainP, 'main-p-offsetY', 'main-p-offsetY-up');
+	// 		break;
+	// 	case wScrollY < mainPtoShow:
+	// 		$addClsls(mainP, 'main-p-offsetY');
+	// 		break;
+	// 	case wScrollY > mainPtoHide:
+	// 		$addClsls(mainP, 'main-p-offsetY-up');
+	// 		break;
+	// 	default:
+	// }
 
 	// set trigger point and get #main-h1
-	let mainH1toShow = 1000;
-	let mainH1toPushUp = 1800;
-	let mainH1toHide = 2000;
+	// let mainH1toShow = 1000;
+	// let mainH1toPushUp = 1800;
+	// let mainH1toHide = 2000;
 	let mainH1 = $select('#main-h1');
-	// check conditions to show/hide main-h1
-	switch (true) {
-		// control #main-h1 show/hide/pushup
-		case wScrollY > mainH1toShow && wScrollY < mainH1toPushUp:
-			$rmClsls(mainH1, 'main-h1-offsetY', 'main-h1-pushup');
-			break;
-		case wScrollY < mainH1toShow:
-			$addClsls(mainH1, 'main-h1-offsetY');
-			break;
-		case wScrollY > mainH1toPushUp && wScrollY < mainH1toHide:
-			$addClsls(mainH1, 'main-h1-pushup');
-			$rmClsls(mainH1, 'main-h1-hide');
-			break;
-		case wScrollY > mainH1toHide:
-			$addClsls(mainH1, 'main-h1-hide')
-			break;
-		default:
+	let articleContainer = $select('#article-container');
+	let articleOffsetTop = articleContainer.offsetTop;
+	let mainContentHide = (wScrollY - window.innerHeight) > (articleOffsetTop * 0.25)
+
+	if (mainContenShow) {
+		$rmClsls(mainH1, 'main-h1-offsetY');
+	} else {
+		$addClsls(mainH1, 'main-h1-offsetY');
 	}
+
+	if (mainContentHide) {
+		$addClsls(mainH1, 'main-h1-offsetY');
+		$addClsls(mainP, 'main-p-offsetY', 'main-p-offsetY-up');
+	}
+
+	// check conditions to show/hide main-h1
+	// switch (true) {
+	// 	// control #main-h1 show/hide/pushup
+	// 	case wScrollY > mainH1toShow && wScrollY < mainH1toPushUp:
+	// 		$rmClsls(mainH1, 'main-h1-offsetY', 'main-h1-pushup');
+	// 		break;
+	// 	case wScrollY < mainH1toShow:
+	// 		$addClsls(mainH1, 'main-h1-offsetY');
+	// 		break;
+	// 	case wScrollY > mainH1toPushUp && wScrollY < mainH1toHide:
+	// 		$addClsls(mainH1, 'main-h1-pushup');
+	// 		$rmClsls(mainH1, 'main-h1-hide');
+	// 		break;
+	// 	case wScrollY > mainH1toHide:
+	// 		$addClsls(mainH1, 'main-h1-hide')
+	// 		break;
+	// 	default:
+	// }
 
 	/**
 	 * article section js
 	**/
 	// set trigger point and get #article-h2
-	let articleTriggerPt = 2000;
 	let articleH2 = $select('#article-h2');
-	if (wScrollY > articleTriggerPt) {
+	// get element move up percentage to trigger event
+	function calTriggerPt(ele, percent) {
+		let eleOffsetTop = ele.offsetTop;
+		let calTriggerPoint = (wScrollY + window.innerHeight) > (eleOffsetTop * percent);
+    // return boolean;
+		return calTriggerPoint;
+	}
+	// let calTriggerPoint = (wScrollY - window.innerHeight) > (eleOffsetTop * percent);
+  //
+	// $log(`article container offset top : ${eleOffsetTop}`);
+	// $log(`wScrollY : ${wScrollY}`);
+	// $log(wScrollY + window.innerHeight);
+	// $log(window.offsetTop);
+
+
+	let articleTriggerPt = calTriggerPt(articleContainer, 1.125);
+
+	if (articleTriggerPt) {
 		$addClsls(articleH2, 'article-h2-scale', 'h2-transform');
 	} else {
 		$rmClsls(articleH2, 'article-h2-scale', 'h2-transform');
 	}
 
-
 	/**
 	 * section begin js
 	**/
   // get section begin wrapper and h1
-	let secBeginWrapper = $select('#section-begin-wrapper');
 	let secBeginH1 = $select('#section-begin-h1');
-  // check if true to hide fixed wrapper
-	if (wScrollY < 2150) {
-		$addClsls(secBeginWrapper, 'hidden');
-	} else if ( wScrollY > 3600 ){
-		$addClsls(secBeginWrapper, 'hidden');
+	let secBeginP = $select('#section-begin-p');
+	let secBeginShow = calTriggerPt(articleContainer, 1.35);
+	let secsecBeginHide = calTriggerPt(articleContainer, 1.65);
+
+	if (secBeginShow) {
+		$rmClsls(secBeginH1, 'sec-begin-h1-offset');
+		$rmClsls(secBeginP, 'sec-begin-p-offset');
 	} else {
-		$rmClsls(secBeginWrapper, 'hidden');
+		$addClsls(secBeginH1, 'sec-begin-h1-offset');
+		$addClsls(secBeginP, 'sec-begin-p-offset');
 	}
+
+	if (secsecBeginHide) {
+		$addClsls(secBeginH1, 'sec-begin-h1-offset');
+		$addClsls(secBeginP, 'sec-begin-p-offset');
+	}
+	// check if true to hide fixed wrapper
+	// if (secBeginShow) {
+	// 	$addClsls(secBeginWrapper, 'hidden');
+	// } else if ( wScrollY > 3600 ){
+	// 	$addClsls(secBeginWrapper, 'hidden');
+	// } else {
+	// 	$rmClsls(secBeginWrapper, 'hidden');
+	// }
   // control section begin h1 movements
-	switch (true) {
-		case wScrollY > 2200 && wScrollY < 2450:
-			$rmClsls(secBeginH1, 'sec-begin-h1-offset', 'sec-begin-h1-pushup');
-			break;
-		case wScrollY < 2200:
-			$addClsls(secBeginH1, 'sec-begin-h1-offset');
-			break;
-		case wScrollY > 2450 && wScrollY < 2850:
-			$rmClsls(secBeginH1, 'sec-begin-h1-topright');
-			$addClsls(secBeginH1, 'sec-begin-h1-pushup');
-			break;
-		case wScrollY > 2850 && wScrollY < 3500:
-			$rmClsls(secBeginH1, 'sec-begin-h1-hide');
-			$addClsls(secBeginH1, 'sec-begin-h1-topright');
-			break;
-		case wScrollY > 3500:
-			$addClsls(secBeginH1, 'sec-begin-h1-hide');
-			break;
-		default:
-	}
+	// switch (true) {
+	// 	case wScrollY > 2200 && wScrollY < 2450:
+	// 		$rmClsls(secBeginH1, 'sec-begin-h1-offset', 'sec-begin-h1-pushup');
+	// 		break;
+	// 	case wScrollY < 2200:
+	// 		$addClsls(secBeginH1, 'sec-begin-h1-offset');
+	// 		break;
+	// 	case wScrollY > 2450 && wScrollY < 2850:
+	// 		$rmClsls(secBeginH1, 'sec-begin-h1-topright');
+	// 		$addClsls(secBeginH1, 'sec-begin-h1-pushup');
+	// 		break;
+	// 	case wScrollY > 2850 && wScrollY < 3500:
+	// 		$rmClsls(secBeginH1, 'sec-begin-h1-hide');
+	// 		$addClsls(secBeginH1, 'sec-begin-h1-topright');
+	// 		break;
+	// 	case wScrollY > 3500:
+	// 		$addClsls(secBeginH1, 'sec-begin-h1-hide');
+	// 		break;
+	// 	default:
+	// }
 
   // get section begin p
-	let secBeginP = $select('#section-begin-p');
+	// let secBeginP = $select('#section-begin-p');
 
   // control section begin p movements
-	switch (true) {
-		case wScrollY < 2475:
-			$addClsls(secBeginP, 'sec-begin-p-offset');
-			break;
-		case wScrollY > 2475 && wScrollY < 2850:
-			$rmClsls(secBeginP, 'sec-begin-p-offset', 'sec-begin-p-push');
-			break;
-		case wScrollY > 2850 && wScrollY < 3100:
-			$addClsls(secBeginP, 'sec-begin-p-push');
-			$rmClsls(secBeginP, 'sec-begin-p-hide');
-			break;
-		case wScrollY > 3100:
-			$addClsls(secBeginP, 'sec-begin-p-hide');
-		default:
-	}
+	// switch (true) {
+	// 	case wScrollY < 2475:
+	// 		$addClsls(secBeginP, 'sec-begin-p-offset');
+	// 		break;
+	// 	case wScrollY > 2475 && wScrollY < 2850:
+	// 		$rmClsls(secBeginP, 'sec-begin-p-offset', 'sec-begin-p-push');
+	// 		break;
+	// 	case wScrollY > 2850 && wScrollY < 3100:
+	// 		$addClsls(secBeginP, 'sec-begin-p-push');
+	// 		$rmClsls(secBeginP, 'sec-begin-p-hide');
+	// 		break;
+	// 	case wScrollY > 3100:
+	// 		$addClsls(secBeginP, 'sec-begin-p-hide');
+	// 	default:
+	// }
 
 	/**
 	 * section landing js
@@ -634,7 +704,7 @@ function checkScroll() {
 
 
 
-	console.log(wScrollY);
+	// console.log(wScrollY);
 
 }
 
