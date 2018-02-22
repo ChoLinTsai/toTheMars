30d08dc9182b2b0566d048fa2937ac780240fa13
Cho Lin Tsai <max19851113@gmail.com>
2018-02-22 15:39:12 +0800

    code beautify

--- webpack.bootstrap.config.js
+++ webpack.bootstrap.config.js
@@ -17,31 +17,26 @@ try {
 
 if (!bootstraprcCustomLocation && !defaultBootstraprcFileExists) {
   /* eslint no-console: 0 */
-  console.log('You did not specify a \'bootstraprc-location\' ' +
-    'arg or a ./.bootstraprc file in the root.');
+  console.log('You did not specify a \'bootstraprc-location\' ' + 'arg or a ./.bootstraprc file in the root.');
   console.log('Using the bootstrap-loader default configuration.');
 }
 
 // DEV and PROD have slightly different configurations
 let bootstrapDevEntryPoint;
 if (bootstraprcCustomLocation) {
-  bootstrapDevEntryPoint = 'bootstrap-loader/lib/bootstrap.loader?' +
-    `configFilePath=${__dirname}/${bootstraprcCustomLocation}` +
-    '!bootstrap-loader/no-op.js';
+  bootstrapDevEntryPoint = 'bootstrap-loader/lib/bootstrap.loader?' + `configFilePath=${__dirname}/${bootstraprcCustomLocation}` + '!bootstrap-loader/no-op.js';
 } else {
   bootstrapDevEntryPoint = 'bootstrap-loader';
 }
 
 let bootstrapProdEntryPoint;
 if (bootstraprcCustomLocation) {
-  bootstrapProdEntryPoint = 'bootstrap-loader/lib/bootstrap.loader?extractStyles' +
-    `&configFilePath=${__dirname}/${bootstraprcCustomLocation}` +
-    '!bootstrap-loader/no-op.js';
+  bootstrapProdEntryPoint = 'bootstrap-loader/lib/bootstrap.loader?extractStyles' + `&configFilePath=${__dirname}/${bootstraprcCustomLocation}` + '!bootstrap-loader/no-op.js';
 } else {
   bootstrapProdEntryPoint = 'bootstrap-loader/extractStyles';
 }
 
 module.exports = {
   dev: bootstrapDevEntryPoint,
-  prod: bootstrapProdEntryPoint,
+  prod: bootstrapProdEntryPoint
 };
--- webpack.config.js
+++ webpack.config.js
@@ -10,140 +10,135 @@ const bootstrapEntryPoints = require('./webpack.bootstrap.config');
 const isProd = process.env.NODE_ENV === 'production'; //true | false
 
 const cssDev = [
-									'style-loader',
-									'css-loader',
-									'sass-loader',
-									{
-										loader: 'sass-resources-loader',
-										options: {
-											resources: './src/css/partials/resources/*.scss'
-										}
-									},
-									'postcss-loader'
-			];
-
+  'style-loader',
+  'css-loader',
+  'sass-loader', {
+    loader: 'sass-resources-loader',
+    options: {
+      resources: './src/css/partials/resources/*.scss'
+    }
+  },
+  'postcss-loader'
+];
 
 const cssProd = ExtractTextPlugin.extract({
-									fallback: 'style-loader',
-									use: [
-													'css-loader',
-													'sass-loader',
-													{
-														loader: 'sass-resources-loader',
-														options: {
-															resources: './src/css/partials/resources/*.scss'
-														}
-													},
-													'postcss-loader'
-									],
-								});
+  fallback: 'style-loader',
+  use: [
+    'css-loader',
+    'sass-loader', {
+      loader: 'sass-resources-loader',
+      options: {
+        resources: './src/css/partials/resources/*.scss'
+      }
+    },
+    'postcss-loader'
+  ]
+});
 
-const cssConfig = isProd ? cssProd : cssDev;
+const cssConfig = isProd
+  ? cssProd
+  : cssDev;
 
-const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
+const bootstrapConfig = isProd
+  ? bootstrapEntryPoints.prod
+  : bootstrapEntryPoints.dev;
 
 module.exports = {
-	entry: {
-		app: './src/js/app.js',
-		bootstrap: bootstrapConfig
-	},
-	output: {
-		path: path.resolve(__dirname, "docs"),
-		filename: './js/[name].bundle.js'
-	},
-	stats: { //optional settings
-		children: false,
-		assets: false,
-		chunks: false,
-		timings: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-				use: cssConfig
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			},
-			{
-				test: /\.(jpe?g|png|gif)$/i,
-				use: [
-						// 'file-loader?name=img/[name].[ext]',
-						// 'image-webpack-loader'
-					{
-							loader: 'file-loader',
-							options: {
-								name: '[name].[ext]',
-								publicPath: '../',
-								outputPath: 'img/'
-							}
-					},
-					{
-						loader: "image-webpack-loader"
-					}
-				]
-			},
-			{
-				test: /\.(woff2?|svg)$/,
-				loader: 'url-loader?limit=10000&name=fonts/[name].[ext]'
-			},
-    	{
-				test: /\.(ttf|eot)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							publicPath: '../',
-							outputPath: 'fonts/'
-						}
-					}
-				]
-				// loader: 'file-loader?name=./fonts/[name].[ext]'
-			},
-			{
-				test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
-				loader: 'imports-loader?jQuery=jquery'
-			},
-		]
-	},
-	devServer: {
-		contentBase: path.join(__dirname, "dist"),
-	  compress: true,
-		// hot: true,
-		stats: "errors-only",
-		// open: true,
-	},
-	plugins: [
-		// new webpack.HotModuleReplacementPlugin(),
-		// new webpack.NamedModulesPlugin(),
-		new HtmlWebpackPlugin({
-			title: 'To The Mars',
-			template: './src/index.html',
-			hash: false,
-			minify: {
-				collapseWhitespace: false
-			}
-		}),
-		new ExtractTextPlugin({
-			filename: './css/[name].css',
-			disable: !isProd,
-			allChunks: true
-		}),
-		// new PurifyCSSPlugin({
+  entry: {
+    app: './src/js/app.js',
+    bootstrap: bootstrapConfig
+  },
+  output: {
+    path: path.resolve(__dirname, "docs"),
+    filename: './js/[name].bundle.js'
+  },
+  stats: { //optional settings
+    children: false,
+    assets: false,
+    chunks: false,
+    timings: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: cssConfig
+      }, {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }, {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: [
+          // 'file-loader?name=img/[name].[ext]',
+          // 'image-webpack-loader'
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              publicPath: '../',
+              outputPath: 'img/'
+            }
+          }, {
+            loader: "image-webpack-loader"
+          }
+        ]
+      }, {
+        test: /\.(woff2?|svg)$/,
+        loader: 'url-loader?limit=10000&name=fonts/[name].[ext]'
+      }, {
+        test: /\.(ttf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              publicPath: '../',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+        // loader: 'file-loader?name=./fonts/[name].[ext]'
+      }, {
+        test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+        loader: 'imports-loader?jQuery=jquery'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    // hot: true,
+    stats: "errors-only",
+    // open: true,
+  },
+  plugins: [
+    // new webpack.HotModuleReplacementPlugin(),
+    // new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'To The Mars',
+      template: './src/index.html',
+      hash: false,
+      minify: {
+        collapseWhitespace: false
+      }
+    }),
+    new ExtractTextPlugin({
+      filename: './css/[name].css',
+      disable: !isProd,
+      allChunks: true
+    }),
+    // new PurifyCSSPlugin({
     //   paths: glob.sync(path.join(__dirname, 'src/*.html')),
     // })
-		// new webpack.ProvidePlugin({
-		// 	$: 'jquery',
+    // new webpack.ProvidePlugin({
+    // 	$: 'jquery',
     //   jQuery: 'jquery',
     //   'window.jQuery': 'jquery',
     //   Popper: ['popper.js', 'default'],
     //   Util: "exports-loader?Util!bootstrap/js/dist/util",
     //   Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-		// 	Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-		// })
-	]
+    // 	Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+    // })
+  ]
 }
